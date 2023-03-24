3b36198936fc3a61f21d2aab3832d947032f6dc4
Zuhrdev <eliechardin.dev@gmail.com>
2023-03-24 15:50:32 +0100

    Create style wityh jss

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import { Greetings } from "./Greetings";
-
 export function App() {
-    return (
-    <>
-        <h1>Hello to the react world</h1>
-        <Greetings 
-        firstName={"Rachel"}
-        age={50}
-        car={{color: "red", vitess: 300}}
-        doSomething={function() {
-            console.log("Hello");
+    const a = true;
+    return <>
+    <div 
+    style={{
+        backgroundColor: "red", 
+        height: a ? 100 : 200, 
+        width: 100,
+        border: "3px solid blue"
         }}
-        isSunny={true}
-        >
-        <img src="https://cdn.pixabay.com/photo/2018/01/21/01/46/architecture-3095716_960_720.jpg"/>
-        </Greetings>
+        ></div>
     </>
-    );
 }
